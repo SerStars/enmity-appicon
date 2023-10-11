ddd4ad8a51762d06a3af90040310e0ac17fb91ca
SerStars <90714930+SerStars@users.noreply.github.com>
2023-10-11 21:20:42 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,31 @@
-import { Plugin, registerPlugin } from 'enmity/managers/plugins'
-import manifest from '../manifest.json'
-import { getByProps } from "enmity/modules"
+import {Plugin, registerPlugin} from 'enmity/managers/plugins'
+import {create} from 'enmity/patcher'
+import manifest, {name as plugin_name} from '../manifest.json'
+import {getByProps} from "enmity/modules"
+// const use = getByProps("canUseAppIcons") // imagine canUseEverything LOL // saving this for future projects
 
 // finally figured getbyprops out
 const icons = getByProps("OFFICIAL_ALTERNATE_ICONS")
 const iconsIds = getByProps("FreemiumAppIconIds")
-const alternateIcons = icons.OFFICIAL_ALTERNATE_ICONS();
-const origFreemiumAppIconIds = iconsIds.FreemiumAppIconIds;
+let alternateIcons = icons.OFFICIAL_ALTERNATE_ICONS()
+
+const Patcher = create('FreeAppIcons')
 
 const FreeAppIcons: Plugin = {
     ...manifest,
 
 //  when the app starts make the icons free for all/each of them
     onStart() {
-        alternateIcons.forEach(icon => (icon._isPremium = icon.isPremium, icon.isPremium = false));
-        icons.ICON.forEach(icon => (icon._isPremium = icon.isPremium, icon.isPremium = false));
+        alternateIcons.forEach(icon => icon.isPremium = false)
+        icons.ICONS.forEach(icon => { icon.isPremium = false });
         iconsIds.FreemiumAppIconIds = iconsIds.MasterAppIconIds;
+
     },
 
 // unpach it all on stop   
     onStop() {
-        alternateIcons.forEach(icon => (icon._isPremium = icon.isPremium, icon.isPremium = true));
-        icons.ICON.forEach(icon => (icon._isPremium = icon.isPremium, icon.isPremium = true));
-        iconsIds.FreemiumAppIconIds = iconsIds.MasterAppIconIds;
+       Patcher.unpatchAll();
     }
  };
- 
+
  registerPlugin(FreeAppIcons);
